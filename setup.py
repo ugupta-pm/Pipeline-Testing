a5ac54df22c439b0d109203560c444809310060c
Umang Gupta <umanggupta@Umangs-MacBook-Pro-2.local>
2020-04-20 13:43:32 -0400

    Adding

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'package',         # How you named your package folder (MyLib)
   packages = ['package'],   # Chose the same as "name"
-  version = '1.1.0',      # Start with a small number and increase it with every change you make
+  version = '1.2.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
